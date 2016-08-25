88bd0c090ad83045eb7afcb9d7fae002db7be976
test <pmcnamee@e112-m-24.mchenry.edu>
2016-08-25 15:53:51 -0500

    Initial Commit

---
+++ Pats First/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pats First
+//
+//  Created by Pat William McNamee on 8/25/16.
+//  Copyright © 2016 Pat William McNamee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
